b83b264b061aeba558bed80258c7732aba979993
Alexandros Papageorgiou <papsoui3@gmail.com>
2026-02-14 20:40:39 +0200

    fdsfd

--- src/App.js
+++ src/App.js
@@ -33,44 +33,6 @@ function App() {
     >
       <ScrollToTop />
 
-      <Helmet>
-        <title>Alex Portfolio | React Developer</title>
-        <meta
-          name="description"
-          content="Explore Alex's portfolio, showcasing React projects, web development skills, and creative solutions."
-        />
-        <meta
-          name="keywords"
-          content="React, Portfolio, Developer, Alex, Web Development"
-        />
-        <meta name="robots" content="index, follow" />
-        <meta
-          name="google-site-verification"
-          content="cINDlTWBfETnc7Dmi5utrfpaGmyzX1quWqweIZXPam8"
-        />
-        {/* Canonical URL */}
-        <link
-          rel="canonical"
-          href={`https://alex-portofolio-site.vercel.app${location.pathname}`}
-        />
-
-        {/* Open Graph / Social Sharing */}
-        <meta property="og:title" content="Alex Portfolio | React Developer" />
-        <meta
-          property="og:description"
-          content="Explore Alex's portfolio, showcasing React projects, web development skills, and creative solutions."
-        />
-        <meta
-          property="og:image"
-          content="https://alex-portofolio-site.vercel.app/og-image.png"
-        />
-        <meta
-          property="og:url"
-          content={`https://alex-portofolio-site.vercel.app${location.pathname}`}
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Helmet>
-
       {/* Conditional Navbar Rendering */}
       {isProjectsRoute ? <NavbarProjects /> : <Navbar />}
 
